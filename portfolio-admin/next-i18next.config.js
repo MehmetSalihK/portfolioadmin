90f86710c4c37e87945de47e72b6a379f5a4f4ef
MehmetSalihK <salihketur60@gmail.com>
2025-01-31 00:32:47 +0100

    test

--- portfolio-admin/next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr', 'en', 'tr'],
-    localeDetection: true,
-  },
-  defaultNS: 'common',
-  localePath: './public/locales',
-};
