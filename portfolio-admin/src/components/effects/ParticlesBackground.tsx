d3907909d50cc5009a6893e029e03c4475b02ffe
MehmetSalihK <salihketur60@gmail.com>
2025-01-31 01:35:57 +0100

    mise a jour navbar

--- portfolio-admin/src/components/effects/ParticlesBackground.tsx
+++ portfolio-admin/src/components/effects/ParticlesBackground.tsx
@@ -9,7 +9,6 @@ export default function ParticlesBackground() {
   }, []);
 
   const particlesLoaded = useCallback(async (container: Container | undefined) => {
-    console.log(container);
   }, []);
 
   return (
--- portfolio-admin/src/components/layout/Layout.tsx
+++ portfolio-admin/src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,12 +9,11 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-grow pt-16">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <Navbar />
+      <div className="pt-16">
         {children}
-      </main>
-      <Footer />
+      </div>
     </div>
   );
 };
